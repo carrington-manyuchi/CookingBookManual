a0fbad0ee5f3c664487a50fdc9e3be3e35c662d5
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-02-13 09:48:59 +0200

    Initial Commit

---
+++ CookingBookManual/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookingBookManual
+//
+//  Created by Manyuchi, Carrington C on 2026/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookingBookManual/CookingBookManualApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingBookManualApp.swift
+//  CookingBookManual
+//
+//  Created by Manyuchi, Carrington C on 2026/02/13.
+//
+
+import SwiftUI
+
+@main
+struct CookingBookManualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
